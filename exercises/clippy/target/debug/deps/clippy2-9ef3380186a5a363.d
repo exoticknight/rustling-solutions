df4718b665385c707250112005a3898575e9f541
exoticknight <draco.knight0@gmail.com>
2021-11-14 22:17:56 +0800

    feat: solutions

---
+++ exercises/clippy/target/debug/deps/clippy2-9ef3380186a5a363.d
@@ -0,0 +1,7 @@
+/data00/home/liangweiwen/project/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-9ef3380186a5a363.rmeta: clippy2.rs
+
+/data00/home/liangweiwen/project/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-9ef3380186a5a363.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
---
+++ exercises/move_semantics/move_semantics5.rs
@@ -0,0 +1,14 @@
+// move_semantics5.rs
+// Make me compile without adding, removing, or changing any of the
+// lines in `main()`.
+// Execute `rustlings hint move_semantics5` for hints :)
+
+
+fn main() {
+    let mut x = 100;
+    let y = &mut x;
+    *y += 100;
+    let z = &mut *y;
+    *z += 1000;
+    assert_eq!(x, 1200);
+}
